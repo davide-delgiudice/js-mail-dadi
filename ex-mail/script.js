f022fd116efa30ad653982ac6fa2a1b91071a840
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-20 15:27:18 +0100

    [UPD] create arr for mail

---
+++ ex-mail/script.js
@@ -0,0 +1,20 @@
+// Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). Si può fare? Certo che si basta ragionare un po’. Nota: Non è necessario provvedere alla validazione delle email
+
+// Creare un array con all'interno diverse mail già esistenti
+// Creare un comando prompt con cui chiedere la mail ad un nuovo utente
+// Creare un ciclo for con cui controllare le mail presenti
+// Creare un if per verificare la condizione vero o falso
+
+
+const eMail = 
+[
+    'davidedelgiudice@gmail.com',
+    'mariateresa@gmail.com',
+    'danielefagiano@gmail.com',
+    'ilariafontana@outlook.com',
+    'danielerossi@outlook.com',
+    'micheleguerra@libero.it',
+    'giulianosangiorgi@gmail.com',
+    'andreasparta@outlook.com'
+]
+
